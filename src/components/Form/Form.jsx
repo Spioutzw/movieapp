331c4d83f4d0cc856eb4317a937be6061928f96e
Roquigny Matthieu <matthieu.roquigny@gmail.com>
2023-06-16 18:55:44 +0200

    Mise en place du style pour login et register

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import style from './Form.module.css';
+import Button from '../Button/Button';
+import Link from 'next/link';
 
-function Form({ title, Message, LoginOrRegister, inputs }) {
+function Form({ title, Message, LoginOrRegister, inputs , textbutton, link }) {
     const schema = yup.object().shape(inputs.reduce((acc, input) => {
         acc[input.name] = input.validation;
         return acc;
@@ -13,18 +16,19 @@ function Form({ title, Message, LoginOrRegister, inputs }) {
     const onSubmit = data => console.log(data);
 
     return (
-        <div>
-            <h3>{title}</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={style.containerForm}>
+            <h3 className={style.h3}>{title}</h3>
+            <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
                 {inputs.map(input => (
                     <div key={input.name}>
-                        <input placeholder={input.placeholder} {...register(input.name)} type={input.type} />
-                        <hr/>
+                            <input className={style.input} placeholder={input.placeholder} {...register(input.name)} type={input.type} />
                         <p>{errors[input.name]?.message}</p>
                     </div>
                 ))}
-                <input type="submit" />
-                <p>{Message}</p> <span>{LoginOrRegister}</span>
+                <Button text={textbutton} />
+                <div className={style.containerPSpan}>
+                <p className={style.p}>{Message}</p> <Link href={link} ><span className={style.span}> &nbsp; {LoginOrRegister}</span></Link>
+                </div>
             </form>
         </div>
     )
