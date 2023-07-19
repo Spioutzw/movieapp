27134054d0fa8b72280349252a06fc3268a69ed2
Roquigny Matthieu <matthieu.roquigny@gmail.com>
2023-07-19 16:49:40 +0200

    ajout gestion erreur upload

--- src/app/api/UploadAvatar/route.js
+++ src/app/api/UploadAvatar/route.js
@@ -6,13 +6,20 @@ import stream from 'stream/promises'
 
 
 export async function POST(request) {
-    const data = await request.formData()
-    const file = data.get('file')
-    const fileName = uuidv4() + '.png'
-    const destination = fs.createWriteStream('public/assets/' + fileName)
-    await stream.pipeline(file.stream(), destination)
-    const imageUrl = `/assets/${fileName}`
 
-    return NextResponse.json({ url: imageUrl })
+    try {
+        const data = await request.formData()
+        const file = data.get('file')
+        const fileName = uuidv4() + '.png'
+        const destination = fs.createWriteStream('public/assets/' + fileName)
+        await stream.pipeline(file.stream(), destination)
+        const imageUrl = `/assets/${fileName}`
+    
+        return NextResponse.json({ url: imageUrl })
+    } catch (error) {
+        // Handle the error here
+        console.error(error)
+    }
+    
   
 }
