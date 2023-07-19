9419a9040df5b263c07fb5b484892193a05f1ca3
Roquigny Matthieu <matthieu.roquigny@gmail.com>
2023-07-19 17:11:12 +0200

    ajout de cloudinary pour l'upload de photo

--- src/app/api/UploadAvatar/route.js
+++ src/app/api/UploadAvatar/route.js
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server'
-import fs from 'fs'
-import { v4 as uuidv4 } from 'uuid'
-import stream from 'stream/promises'
+import { v2 as cloudinary } from 'cloudinary';
 
+cloudinary.config({
+    cloud_name: 'dmzr4omwv',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 
 export async function POST(request) {
@@ -10,16 +13,24 @@ export async function POST(request) {
     try {
         const data = await request.formData()
         const file = data.get('file')
-        const fileName = uuidv4() + '.png'
-        const destination = fs.createWriteStream('public/assets/' + fileName)
-        await stream.pipeline(file.stream(), destination)
-        const imageUrl = `/assets/${fileName}`
-    
+        const result = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream((error, result) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
+            file.stream.pipe(stream)
+        })
+
+        const imageUrl = result.secure_url
+
         return NextResponse.json({ url: imageUrl })
     } catch (error) {
         // Handle the error here
         console.error(error)
     }
-    
-  
+
+
 }
