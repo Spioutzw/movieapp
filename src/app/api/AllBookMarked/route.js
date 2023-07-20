c055f94a2c67e04299db4574100887971a75a94d
Roquigny Matthieu <matthieu.roquigny@gmail.com>
2023-07-20 09:27:53 +0200

    ajout debug bookmarked

--- src/app/api/AllBookMarked/route.js
+++ src/app/api/AllBookMarked/route.js
@@ -3,12 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     // find all movie with trending true
-    const trendingFilms = await prisma.media.findMany({
+    const bookMarkedFilmsSeries = await prisma.media.findMany({
         where: {
             isBookmarked: true
         }
     });
 
 
-    return NextResponse.json(trendingFilms);
+    console.log(bookMarkedFilmsSeries,'bookMarkedFilmsSeries');
+
+
+
+    return NextResponse.json(bookMarkedFilmsSeries);
 }
